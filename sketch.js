cb95aada58cf0289b9466b4ce91b19bc41f09699
Ethan Bresnick <ethanbresnick@gmail.com>
2020-04-03 00:53:02 -0700

    fix

--- sketch.js
+++ sketch.js
@@ -1,37 +1,47 @@
 var img;
 var c;
 
-function preload()
-{
- img = loadImage("http://dev.ethanbresnick.com/data/colors.jpg");
-}
+//function preload()
+//{
+//img = loadImage("http://dev.ethanbresnick.com/data/colors.jpg");
+//}
 
 function setup() {
-    createCanvas(width, height);
+    createCanvas(1280, 720);
     c = color(255,0,0);
 }
 
 function draw() {
-  image(img, 0, 0);
+//  image(img, 0, 0);
 
-    strokeWeight(3);
-    stroke(255);
+    noStroke();
     fill(c);
     rect(25, 25, 25, 25);
+
+
 }
 
-function mousePressed()
-{
-    c = get(mouseX, mouseY);
+function keyTyped() {
+  if (key === 'a') {
+    c = color(255,0,0);;
+  }
+  if (key === 'b') {
+    c = color(0,255,0);;
+  }
 
-    var rgbValue = c[0] + "," + c[1] + "," + c[2];
-    print(rgbValue);
+  background (c);
 
-    var particle = new Particle();
-    particle.callFunction({
-       deviceId: '1f0039000347363339343638',
-       name: 'led',
-       argument: rgbValue,
-       auth: 'f39e032ceebe32a5c445a7b29eabab9dcf585ede'
-    });
+
+c = get(mouseX, mouseY);
+
+      var rgbValue = c[0] + "," + c[1] + "," + c[2];
+      print(rgbValue);
+
+      var particle = new Particle();
+      particle.callFunction({
+         deviceId: '1f0039000347363339343638',
+         name: 'led',
+         argument: rgbValue,
+         auth: 'f39e032ceebe32a5c445a7b29eabab9dcf585ede'
+      });
 }
